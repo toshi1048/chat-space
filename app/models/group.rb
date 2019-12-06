18764a896e065c97fad5a291882ab9ce29b15682
toshi1048 <toshiya.22.1048@gmail.com>
2019-12-06 17:23:43 +0900

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,8 @@
 class Group < ApplicationRecord
+<<<<<<< Updated upstream
   has_many :group_users
   has_many :users, through: :group_users
   validates :name, presence: true, uniqueness: true
+=======
+>>>>>>> Stashed changes
 end
