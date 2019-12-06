dd53f44c64a0992bac0e557fc43af93f58207f99
toshi1048 <toshiya.22.1048@gmail.com>
2019-12-06 16:09:12 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @messages = message.all
+  end
+
+  def new
+    @message = message.new
+  end
+
+  def create
+    Message.create(message_params[:id])
+  end
+
+  def edit
+    @message = Message.find(params[:id])
+  end
+
+  def update
+    message = message.find(params[:id])
+    message.update(message_params)
   end
 end
