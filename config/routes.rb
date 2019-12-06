20398ae5b96e1cf6a6084921cf3592e06567513e
toshi1048 <toshiya.22.1048@gmail.com>
2019-12-06 17:23:56 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
