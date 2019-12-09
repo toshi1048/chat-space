4e2c2ebd1b82c93dbf193dcdbcd4c1c8be54f962
toshi1048 <toshiya.22.1048@gmail.com>
2019-12-09 18:57:32 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< Updated upstream
          :recoverable, :rememberable, :validatable
-=======
-         :recoverable, :rememberable,  :validatable
->>>>>>> Stashed changes
 
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
-<<<<<<< Updated upstream
 end
-=======
-end
->>>>>>> Stashed changes
